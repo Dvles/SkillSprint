96babf28b406a48e556c6f5d065e019c3e373c17
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-04 19:45:35 +0100

    Added FakeDB with sample User data for simulating database

--- DAL/Repositories/FakeDB.cs
+++ DAL/Repositories/FakeDB.cs
@@ -6,7 +6,45 @@ using DAL.Entities;
 namespace DAL.Repositories{
     public static class FakeDB{
         public static List<User> Users = new List<User>{
-            
-        }
+
+            new User (
+                
+                "Sabrina",
+                "Manhilo",
+                "user1@gmail.com",
+                "0000"
+               
+            ),
+
+            new User (
+                
+                "Kelly",
+                "Grander",
+                "user2@gmail.com",
+                "0000"
+               
+            ),
+
+            new User (
+                
+                "Devota",
+                "Twagyra",
+                "user3@gmail.com",
+                "0000"
+               
+            ),
+
+            new User (
+                
+                "Ben",
+                "Delcroix",
+                "user4@gmail.com",
+                "0000"
+               
+            ),
+
+
+
+        };
     }
 }

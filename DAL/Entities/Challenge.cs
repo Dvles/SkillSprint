b1b37b3da40e6eff496c0d8be7bb4b1bcfd8a2ce
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-04 19:57:39 +0100

    DAL -> Create Challenge entity

---
+++ DAL/Entities/Challenge.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace DAL.Entities {
+
+    public class Challenge{
+
+        public Guid Challenge_Id {get;set;}
+        public string Title {get;set;}
+        public string Description {get;set;}
+        public DateTime StartDate {get;set;}
+        public DateTime EndDate {get;set;}
+        public ChallengeCategory Category {get;set;}
+
+
+
+    }
+
+    public enum ChallengeCategory
+    {
+        [Description("Create music pieces")]
+        Music,
+        
+        [Description("Write beautiful poetry")]
+        Poetry,
+        
+        [Description("Develop coding challenges")]
+        Coding,
+        
+        [Description("Design creative works")]
+        Design
+    }
+
+}

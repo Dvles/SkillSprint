26c4cd6696fae2377b40cdb268ffcd3605307136
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-17 15:48:07 +0100

    Created Mapper class with ToUser extension method to map IDataRecord to User entity. Added null-check for record input and obfuscated password field

---
+++ DAL/Mapper/Mapper.cs
@@ -0,0 +1,27 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Mapper
+{
+    internal static class Mapper
+    {
+		public static User ToUser(this IDataRecord record)
+		{
+			if (record is null) throw new ArgumentNullException(nameof(record));
+			//if (record is null) return null;
+			return new User()
+			{
+				User_Id = (Guid)record[nameof(User.User_Id)],
+				First_Name = (string)record[nameof(User.First_Name)],
+				Last_Name = (string)record[nameof(User.Last_Name)],
+				Email = (string)record[nameof(User.Email)],
+				Password = "********",
+			};
+		}
+	}
+}
